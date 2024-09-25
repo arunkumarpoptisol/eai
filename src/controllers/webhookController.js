476538c67987fa29e86f325ebab96297a2d5f4eb
arunkennedy <arun@kennedytechgroup.com>
2024-09-25 11:30:31 +0530

    added logs

--- src/controllers/webhookController.js
+++ src/controllers/webhookController.js
@@ -6,6 +6,16 @@ const { getShopifyExpression } = require("../expression/shopifyExpression");
 // const { transformData } = require("../services/transformService");
 // const sendMessage = require("../queue/sendMessage");
 
+/**
+ * Verifies the authenticity of incoming Shopify webhook requests by comparing the HMAC-SHA256 signature.
+ *
+ * @param {Object} req - The Express request object containing the incoming webhook data.
+ * @param {Object} res - The Express response object to send a response back to Shopify.
+ * @param {Function} next - The next middleware function in the Express application.
+ *
+ * @returns {void} Calls the next middleware function if the verification is successful,
+ * or sends an unauthorized response if the verification fails.
+ */
 exports.verifyShopifyWebhook = (req, res, next) => {
   const hmacHeader = req.get("X-Shopify-Hmac-Sha256");
   const generatedHash = crypto
@@ -20,24 +30,41 @@ exports.verifyShopifyWebhook = (req, res, next) => {
   }
 };
 
+/**
+ * Handles incoming Shopify webhook requests and processes the data.
+ *
+ * @param {Object} req - The Express request object containing the incoming webhook data.
+ * @param {Object} res - The Express response object to send a response back to Shopify.
+ *
+ * @returns {Promise<void>} Resolves when the webhook processing is complete or rejects with an error.
+ */
 exports.handleShopifyWebhook = async (req, res) => {
   try {
-    const shopifyData = req.body;
-    const tempConfig = config.rabbitmq;
+    const shopifyData = req.body; // The incoming Shopify webhook data.
+    const tempConfig = config.rabbitmq; // The RabbitMQ configuration.
+
+    // Log the incoming Shopify data.
     await Log.create({
       phase: "shopify",
       data: shopifyData,
     });
+
+    // Transform the incoming Shopify data using the getShopifyExpression function.
     const transformedData = await getShopifyExpression(shopifyData);
 
+    // Log the transformed data.
     await Log.create({
       phase: "transform",
       data: transformedData,
     });
-    // Process the incoming Shopify data
+
+    // Process the transformed data by sending it to RabbitMQ.
     await sendMessage(transformedData, tempConfig);
+
+    // Send a success response back to Shopify.
     res.status(200).send("success");
   } catch (error) {
+    // If an error occurs during processing, send an error response back to Shopify.
     res.status(500).send(error.message);
   }
 };
